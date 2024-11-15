4d176630da4ef7b2054936e3023f2027abb7877f
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-11-15 09:27:06 -0800

    wIP about section stlying

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,9 +1,14 @@
 const parser = new DOMParser();
+const body = document.querySelector("body");
 const main = document.querySelector("main");
+const popover = document.querySelector("#mobile-popover-menu");
 const templates = {
   "web-design": await extractTemplate("web-design"),
   "app-design": await extractTemplate("app-design"),
   "graphic-design": await extractTemplate("graphic-design"),
+  about: await extractTemplate("about"),
+  locations: await extractTemplate("locations"),
+  contact: await extractTemplate("contact"),
 };
 
 async function extractTemplate(id) {
@@ -12,14 +17,16 @@ async function extractTemplate(id) {
     .querySelector(`#${id}-template`);
 }
 
-main.addEventListener("click", (e) => {
+body.addEventListener("click", (e) => {
   e.preventDefault();
   const goToBtn = e.target.closest("[data-go-to]");
-  console.log(e.target);
+  const popoverBtn = e.target.closest("[popovertarget]");
 
   if (goToBtn) {
     console.log(templates[goToBtn.dataset.goTo]);
     const clone = templates[goToBtn.dataset.goTo].content.cloneNode(true);
     main.replaceChildren(clone);
+  } else if (popoverBtn) {
+    popover.togglePopover();
   }
 });
