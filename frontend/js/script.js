fa11c908013e991de96adec48fff7cf82dc08ac2
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-11-16 09:15:40 -0800

    logic for input contact

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -12,6 +12,33 @@ const templates = {
   home: await extractTemplate("home"),
 };
 
+const config = { childList: true };
+const callback = (mutationList, observer) => {
+  for (const mutation of mutationList) {
+    if (
+      mutation.type === "childList" &&
+      mutation.target.querySelector('[data-page="contact"]')
+    ) {
+      const contactForm = mutation.target
+        .querySelector('[data-page="contact"]')
+        .querySelector("form.contact-form");
+      const contactInputs = contactForm.querySelectorAll("input, textarea");
+      for (const contactInput of contactInputs) {
+        contactInput.addEventListener("input", checkIfEmpty);
+      }
+    }
+  }
+};
+
+function checkIfEmpty(e) {
+  const input = e.target;
+  if (input.nextElementSibling) {
+    input.nextElementSibling.classList.toggle("hide", input.value != "");
+  }
+}
+const observer = new MutationObserver(callback);
+observer.observe(main, config);
+
 async function extractTemplate(id) {
   return parser
     .parseFromString(await (await fetch(`./${id}.html`)).text(), "text/html")
@@ -24,7 +51,7 @@ body.addEventListener("click", (e) => {
   const popoverBtn = e.target.closest("[popovertarget]");
 
   if (goToBtn) {
-    console.log(templates[goToBtn.dataset.goTo]);
+    // console.log(templates[goToBtn.dataset.goTo]);
     const clone = templates[goToBtn.dataset.goTo].content.cloneNode(true);
     main.replaceChildren(clone);
   } else if (popoverBtn) {
